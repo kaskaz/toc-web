0afd18aa87213583c55c267cdf2ef70a3e2fd8b2
Nuno Cascalho <nuno.cascalho@digitalhub.man>
2021-08-04 01:43:56 +0100

    update footer style and add twitter widget script

---
+++ the-original-cover/src/components/FollowTwitterButton.tsx
@@ -0,0 +1,13 @@
+const FollowTwitterButton = () => {
+    return (
+        <div>
+            <a  className="twitter-follow-button" 
+                data-show-count='false' 
+                href="https://twitter.com/originalvscover">
+                Follow @originalvscover
+            </a>
+        </div>
+    );
+};
+
+export default FollowTwitterButton;
--- the-original-cover/src/components/Footer.tsx
+++ the-original-cover/src/components/Footer.tsx
@@ -1,20 +1,61 @@
-import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, IconButton, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import { FileCopy } from "@material-ui/icons";
+import FollowTwitterButton from "./FollowTwitterButton";
+import ClipboardJS from "clipboard";
+import { useEffect } from "react";
 
 const useStyles = makeStyles({
     appBar: {
         top: 'auto',
-        bottom: 0
+        bottom: 0,
+        color: '#f6eeee',
+        backgroundColor: '#363636'
+    },
+    toolbar: { 
+        display: 'flex',
+        justifyContent: 'space-around'
+    },
+    contact: { 
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    '@media (max-width: 450px)': {
+        toolbar: {
+            flexDirection: 'column',
+            gap: '5px',
+            margin: '5px 0px'
+        }
     }
 });
 
 const Footer = () => {
     const classes = useStyles();
+
+    useEffect(() => {
+        new ClipboardJS("#contact-address");
+    }, []);
+
     return (
         <AppBar position="fixed" color="primary" className={classes.appBar}>
-            <Toolbar>
-                <Typography variant="h6">
-                    Footer
-                </Typography>
+            <Toolbar className={classes.toolbar}>
+                <div className={classes.contact}>
+                    <Typography variant="caption">
+                        contact us
+                    </Typography>
+                    <div style={{ display: 'flex', alignItems: 'center'}}>
+                        <IconButton 
+                            style={{ padding: 0, marginRight: 5 }}
+                            id="contact-address"
+                            data-clipboard-text="contact@theoriginalcover.com">
+                            <FileCopy fontSize="small" htmlColor="#f6eeee"/>
+                        </IconButton>
+                        <Typography>
+                            contact@theoriginalcover.com
+                        </Typography>
+                    </div>
+                </div>
+                <FollowTwitterButton />
             </Toolbar>
         </AppBar>
     );
